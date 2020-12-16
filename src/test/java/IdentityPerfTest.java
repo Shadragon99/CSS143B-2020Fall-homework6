24c9917f887b5e6155148e33f64ae5b7cb642eb6
Haaris Nadeem <haaris2001@gmail.com>
2020-12-16 21:39:11 +0500

    Graph and Analysis has been made and dictionary is also complete.

--- src/test/java/IdentityPerfTest.java
+++ src/test/java/IdentityPerfTest.java
@@ -40,7 +40,7 @@ public abstract class IdentityPerfTest {
     @Test
     public void runTest() {
         // timing method based on https://howtodoinjava.com/java/date-time/execution-elapsed-time/
-        for (int i = 5000; i < 1000000; i += 5000) {
+        for (int i = 5000; i < 5000; i += 5000) {
             // homework
 
             runTestOfSize(i);
